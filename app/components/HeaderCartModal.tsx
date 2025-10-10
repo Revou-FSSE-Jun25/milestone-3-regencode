9217f16b9e1439538434d83e69bba57004e3dc21
regencode <thomgozalie@gmail.com>
2025-10-10 12:52:53 +0700

    fix: fix error for cartModal

--- app/components/HeaderCartModal.tsx
+++ app/components/HeaderCartModal.tsx
@@ -8,10 +8,9 @@ const HeaderCartModal = () => {
     const { cartItems, addCartItems, removeCartItems } = useContext(cartContext);
     return (
         <div className="fixed z-10 bg-white/30 h-[100vh] w-[100vw] text-white">
-        { [...cartItems.entries()].map(([product, quantity]) => (
-            <div key={product.id}>
-                <h1>{product.title}</h1>
-                <h1>{quantity}</h1>
+        { [...cartItems.entries()].map(([id, quantity]) => (
+            <div key={id}>
+                <h1>{id} : {quantity}</h1>
             </div>
         )) }
         </div>
