14e34d11573f421340e0b097473aa632547ac161
regencode <thomgozalie@gmail.com>
2025-10-10 12:56:56 +0700

    fix: use string type for dates

--- app/types.tsx
+++ app/types.tsx
@@ -1,22 +1,22 @@
 import { Url } from "next/dist/shared/lib/router/router"
 
 export interface Category {
-    creationAt: Date,
+    creationAt: string,
     id: number | string | undefined,
     image: string,
     name: string,
     slug: string,
-    updatedAt: string | Date,
+    updatedAt: string,
 }
 
 export interface Product {
     category: Category,
-    creationAt: Date,
+    creationAt: string,
     description: string
     id: number | string,
     images: Array<string>, 
     price: number,
     slug: string,
     title: string,
-    updatedAt: string | Date, 
+    updatedAt: string, 
 }
